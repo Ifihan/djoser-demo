84fc528fe7921cf5bfa106ee7265ba932cce71d5
Ifihan <victoriaolusheye@gmail.com>
2022-02-23 16:56:39 +0100

    First commit

---
+++ authenticate/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from djoser.views import UserViewSet
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+app_name = "authenticate"
+
+urlpatterns = [
+	path('register/', UserViewSet.as_view({'post': 'create'}), name="register"),
+	path("login/", TokenObtainPairView.as_view(), name="login"),
+	path("resend-activation/", UserViewSet.as_view({"post": "resend_activation"}), name="resend_activation"),
+	path("activation/<str:uid>/<str:token>/", UserViewSet.as_view({"post": "activate"}), name="activate"),
+	path("reset-password/", UserViewSet.as_view({"post": "reset_password"}), name="reset_password"),
+	path("reset-password-confirm/<str:uid>/<str:token>/", UserViewSet.as_view({"post": "reset_password_confirm"}), name="reset_password_confirm"),
+]
